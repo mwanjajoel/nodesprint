2b0445face76b8d2fff9b0f9a8676d7473b95d5c
mwanjajoel <joelsilverworks@gmail.com>
2017-06-25 15:16:51 +0300

    First Node JS code, woohooo!

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mwanjajoel on 25/06/2017.
+ */
+
+var time = 0;
+
+//set a timer variable that runs a setInterval time function
+var timer = setInterval(function () {
+
+    time += 2;
+
+    console.log(time + " seconds have passed");
+
+    //check whether the time is more than 10 seconds
+    if (time > 10) {
+
+        //if it is, then we clear the timer thus stopping the function.
+        clearInterval(timer);
+    }
+
+}, 2000);
